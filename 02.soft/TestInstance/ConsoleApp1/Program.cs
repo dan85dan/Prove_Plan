df81f54ea67f3c0615b581a6a13854a2992cf3c6
yayawunian <251472910@qq.com>
2017-07-09 00:34:44 +0800

    int强制类型转换特点

---
+++ 02.soft/TestInstance/ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] nums = { 3.2, 3.5, 3.6 };
+            foreach(double num in nums)
+            {
+                Console.WriteLine("{0} -> {1}", num, (int)num);
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
