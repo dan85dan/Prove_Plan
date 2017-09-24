09eb15994f58999976feb5f22a20df3d5201b108
yayawunian <251472910@qq.com>
2017-09-24 17:29:46 +0800

    新增chaptor10

---
+++ 02.soft/Wrox/Wrox/Wrox/chaptor10/EmployTd.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wrox.chaptor10
+{
+    public class EmployeeIdException : Exception
+    {
+        public EmployeeIdException(string msg) : base(msg) { }
+    }
+    public struct EmployTd
+    {
+        public readonly char Prefix;
+        public readonly int number;
+
+        public EmployTd(string id)
+        {
+            if (id == null) throw new ArgumentNullException("id");
+
+            Prefix = (id.ToUpper())[0];
+        }
+    }
+}
---
+++ 02.soft/Wrox/Wrox/Wrox/chaptor10/Generics.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wrox.chaptor10
+{
+    class Generics
+    {
+        public static void Test1()
+        {
+            var books = new SortedList<string, string>();
+            books.Add("C#", "01");
+            books.Add("Java", "02");
+            books["C++"] = "03";
+            books["C"] = "04";
+
+            Console.WriteLine("show all");
+            foreach(KeyValuePair<string,string> book in books)
+            {
+                Console.WriteLine("{0} - {1}", book.Key, book.Value);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("only show key");
+            foreach(string key in books.Keys)
+            {
+                Console.WriteLine(key);
+            }
+
+            Console.WriteLine("\nonly show value");
+            foreach(string value in books.Values)
+            {
+                Console.WriteLine(value);
+            }
+        }
+    }
+}
